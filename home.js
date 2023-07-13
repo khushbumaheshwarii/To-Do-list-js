60090a63f6504e4fce7100a0da73c88f89133f72
khushbumaheshwarii <khushbumaheshwari007@gmail.com>
2023-07-13 19:55:10 +0530

    addItem function working

--- home.js
+++ home.js
@@ -1,29 +1,28 @@
 const Item = document.getElementById('submit');
-const addItem = document.getElementById('items');
-Item.addEventListener('click',addItem);
+const addItem = document.querySelector('.items');
+const lis = document.querySelector('li');
+Item.addEventListener('click',addItems);
+addItem.addEventListener('click', removeItem);
 
-function addItem(e){
+function addItems(e){
      e.preventDefault();
-
      const newItem = document.getElementById('add-item').value;
-
      const li = document.createElement('li');
-
      li.appendChild(document.createTextNode(newItem));
-
-     var deleteBtn = document.createElement('button');
-
+     var deleteBtn = document.createElement('i');
+     deleteBtn.appendChild(document.createTextNode(''));
      deleteBtn.className = "fa-solid fa-x";
-
-     deleteBtn.appendChild(document.createTextNode());
      addItem.appendChild(li);
+     li.appendChild(deleteBtn);
+     }
+
+
+function removeItem(e){
+     if(e.target.classList.contains('fa-x')){
+          if(confirm('Are you Sure?')){
+               var li = e.target.parentElement;
+               DataTransferItemList.removeChild(li);
+          }
+     }
 }
 
-// function removeItem(e){
-//      if(e.target.classList.contains('delete')){
-//           if(confirm('Are you Sure?')){
-//                var li = e.target.parentElement;
-//                DataTransferItemList.removeChild(li);
-//           }
-//      }
-// }
